087d115c3934f16af9f0f6c1a3c9124ed3f67685
Tristan Blaudez <tblaudez@student.codam.nl>
2021-10-13 16:37:51 +0200

    Fixed Sully to comply with evaluation

--- ASM/Sully.asm
+++ ASM/Sully.asm
@@ -1,10 +1,11 @@
 section .data
-data: db "section .data%3$cdata: db %4$c%1$s%4$c, 0%3$cfilename_buffer: db 16%3$cfilename: db %4$cSully_%5$cd.asm%4$c, 0%3$copen_mode: db %4$cw+%4$c, 0%3$ccommand_buffer: db 64%3$ccommand: db %4$cnasm -fmacho64 Sully_%5$c1$d.asm && clang Sully_%5$c1$d.o -o Sully_%5$c1$d && ./Sully_%5$c1$d%4$c, 0%3$c%3$csection .text%3$cglobal _main%3$cextern _fopen%3$cextern _fclose%3$cextern _sprintf%3$cextern _fprintf%3$cextern _system%3$c%3$c_main:%3$cpush r12%3$cmov r12, %2$d%3$ccmp r12, 0%3$cjle _done%3$cdec r12%3$c%3$clea rdi, [rel filename_buffer]%3$clea rsi, [rel filename]%3$cmov rdx, r12%3$ccall _sprintf%3$c%3$clea rdi, [rel filename_buffer]%3$clea rsi, [rel open_mode]%3$ccall _fopen%3$cpush rax%3$c%3$cmov rdi, rax%3$clea rsi, [rel data]%3$clea rdx, [rel data]%3$cmov rcx, r12%3$cmov r8, 10%3$cmov r9, 34%3$cpush 37%3$ccall _fprintf%3$cpop rdi%3$c%3$cpop rdi%3$ccall _fclose%3$c%3$clea rdi, [rel command_buffer]%3$clea rsi, [rel command]%3$cmov rdx, r12%3$ccall _sprintf%3$c%3$clea rdi, [rel command_buffer]%3$ccall _system%3$c%3$c_done:%3$cpop r12%3$cret", 0
+data: db "section .data%3$cdata: db %4$c%1$s%4$c, 0%3$cfilename_buffer: db 16%3$cfilename: db %4$cSully_%5$cd.asm%4$c, 0%3$copen_mode: db %4$cw+%4$c, 0%3$ccommand_buffer: db 64%3$ccompile_command: db %4$cnasm -fmacho64 Sully_%5$c1$d.asm && clang Sully_%5$c1$d.o -o Sully_%5$c1$d%4$c, 0%3$crun_command: db %4$c./Sully_%5$c1$d%4$c, 0%3$c%3$csection .text%3$cglobal _main%3$cextern _fopen%3$cextern _fclose%3$cextern _sprintf%3$cextern _fprintf%3$cextern _system%3$c%3$c_main:%3$cpush r12%3$cmov r12, %2$d%3$cdec r12%3$c%3$clea rdi, [rel filename_buffer]%3$clea rsi, [rel filename]%3$cmov rdx, r12%3$ccall _sprintf%3$c%3$clea rdi, [rel filename_buffer]%3$clea rsi, [rel open_mode]%3$ccall _fopen%3$cpush rax%3$c%3$cmov rdi, rax%3$clea rsi, [rel data]%3$clea rdx, [rel data]%3$cmov rcx, r12%3$cmov r8, 10%3$cmov r9, 34%3$cpush 37%3$ccall _fprintf%3$cpop rdi%3$c%3$cpop rdi%3$ccall _fclose%3$c%3$clea rdi, [rel command_buffer]%3$clea rsi, [rel compile_command]%3$cmov rdx, r12%3$ccall _sprintf%3$c%3$clea rdi, [rel command_buffer]%3$ccall _system%3$c%3$ccmp r12, -1%3$cjle _done%3$c%3$clea rdi, [rel command_buffer]%3$clea rsi, [rel run_command]%3$cmov rdx, r12%3$ccall _sprintf%3$c%3$clea rdi, [rel command_buffer]%3$ccall _system%3$c%3$c_done:%3$cpop r12%3$cret", 0
 filename_buffer: db 16
 filename: db "Sully_%d.asm", 0
 open_mode: db "w+", 0
 command_buffer: db 64
-command: db "nasm -fmacho64 Sully_%1$d.asm && clang Sully_%1$d.o -o Sully_%1$d && ./Sully_%1$d", 0
+compile_command: db "nasm -fmacho64 Sully_%1$d.asm && clang Sully_%1$d.o -o Sully_%1$d", 0
+run_command: db "./Sully_%1$d", 0
 
 section .text
 global _main
@@ -17,8 +18,6 @@ extern _system
 _main:
 push r12
 mov r12, 5
-cmp r12, 0
-jle _done
 dec r12
 
 lea rdi, [rel filename_buffer]
@@ -45,7 +44,18 @@ pop rdi
 call _fclose
 
 lea rdi, [rel command_buffer]
-lea rsi, [rel command]
+lea rsi, [rel compile_command]
+mov rdx, r12
+call _sprintf
+
+lea rdi, [rel command_buffer]
+call _system
+
+cmp r12, -1
+jle _done
+
+lea rdi, [rel command_buffer]
+lea rsi, [rel run_command]
 mov rdx, r12
 call _sprintf
 
--- C/Sully.c
+++ C/Sully.c
@@ -2,13 +2,15 @@
 #include <stdlib.h>
 int main() {
 int i=5;
-if (i<=0) return 0;
-char data[]="#include <stdio.h>%3$c#include <stdlib.h>%3$cint main() {%3$cint i=%2$d;%3$cif (i<=0) return 0;%3$cchar data[]=%4$c%1$s%4$c;%3$cchar filename[16];%3$csprintf(filename,%4$cSully_%5$cd.c%4$c,i-1);%3$cFILE *file=fopen(filename,%4$cw+%4$c);%3$cfprintf(file,data,data,i-1,10,34,37);%3$cfclose(file);%3$cchar command[64];%3$csprintf(command,%4$cclang -Wall -Wextra -Werror Sully_%5$c1$d.c -o Sully_%5$c1$d && ./Sully_%5$c1$d%4$c,i-1);%3$csystem(command);}";
+char data[]="#include <stdio.h>%3$c#include <stdlib.h>%3$cint main() {%3$cint i=%2$d;%3$cchar data[]=%4$c%1$s%4$c;%3$cchar filename[16];%3$csprintf(filename,%4$cSully_%5$cd.c%4$c,i-1);%3$cFILE *file=fopen(filename,%4$cw+%4$c);%3$cfprintf(file,data,data,i-1,10,34,37);%3$cfclose(file);%3$cchar command[64];%3$csprintf(command,%4$cclang -Wall -Wextra -Werror Sully_%5$c1$d.c -o Sully_%5$c1$d%4$c,i-1);%3$csystem(command);%3$cif (i<=0) return 0;%3$csprintf(command,%4$c./Sully_%5$cd%4$c,i-1);%3$csystem(command);}";
 char filename[16];
 sprintf(filename,"Sully_%d.c",i-1);
 FILE *file=fopen(filename,"w+");
 fprintf(file,data,data,i-1,10,34,37);
 fclose(file);
 char command[64];
-sprintf(command,"clang -Wall -Wextra -Werror Sully_%1$d.c -o Sully_%1$d && ./Sully_%1$d",i-1);
+sprintf(command,"clang -Wall -Wextra -Werror Sully_%1$d.c -o Sully_%1$d",i-1);
+system(command);
+if (i<=0) return 0;
+sprintf(command,"./Sully_%d",i-1);
 system(command);}
